4871386a9ddd240dcd7958cdfb9181739a81d04e
Алиса Скрынько <skrynko.a@ad.almara.org>
2021-02-16 09:34:57 +0300

    First commit.

---
+++ Ololo1.App/SimpleClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class SimpleClass
+    {
+        public static bool CheckTextFromConsole(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("You haven't not entered anything");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("You entered: " + text);
+                return true;
+            }
+        }
+    }
+}
---
+++ Ololo1.Tests/CheckTextFromConsoleTests.cs
@@ -0,0 +1,25 @@
+using ConsoleApp1;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        [Test]
+        public void CheckTextFromConsoleByEmptyStringTesting(string text)
+        {
+            var result = SimpleClass.CheckTextFromConsole(text);
+            Assert.IsFalse(result, "Для значения " +  text + " метод отработал некорректно");
+        }
+        
+        [TestCase("jjdfhddgdf")]
+        [Test]
+        public void CheckTextFromConsoleByNonEmptyStringTesting(string text)
+        {
+            var result = SimpleClass.CheckTextFromConsole(text);
+            Assert.IsTrue(result, "Для значения " +  text + " метод отработал некорректно");
+        }
+    }
+}
